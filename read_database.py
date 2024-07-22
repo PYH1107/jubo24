2aa72bd6d48ed5b1f79eac8ef6c8cc0ff6023482
wilburkwan <wilburkwan@gmail.com>
2024-07-22 13:46:21 +0800

    feat(): add get mord info

--- read_database.py
+++ read_database.py
@@ -31,11 +31,10 @@ class JSONEncoder(json.JSONEncoder):
 def filter_empty_fields(doc):
     return {k: v for k, v in doc.items() if v}
 
-# 查找並返回符合條件的 patients 集合中的文檔
 def read_health_data():
     collection_name = "patients"
     collection = database[collection_name]
-    query = {"lastName": "董", "firstName": {"$regex": ".*萱.*"}}
+    query = {"lastName": "張小", "firstName": {"$regex": ".*小英.*"}}
     documents = collection.find(query)
     print(f"Collection: {collection_name}")
     if collection.count_documents(query) == 0:
@@ -47,7 +46,6 @@ def read_health_data():
             print(json.dumps(filtered_doc, ensure_ascii=False, indent=4, cls=JSONEncoder))
             return doc["_id"]
 
-# 查找並打印 vitalsigns 集合中的文檔
 def read_vital_signs(patient_id, start_date, end_date):
     query = {
         "patient": ObjectId(patient_id),
@@ -105,12 +103,53 @@ def read_nursingnotedetails(patient_id, start_date, end_date):
             filtered_doc = filter_empty_fields(doc)
             print(json.dumps(filtered_doc, ensure_ascii=False, indent=4, cls=JSONEncoder))
 
+def read_nursingdiagnoses(patient_id, start_date, end_date):
+    collection_name = "nursingdiagnoses"
+    collection = database[collection_name]
+    query = {
+        "patient": ObjectId(patient_id),
+        "createdDate": {
+            "$gte": datetime.datetime.strptime(start_date, "%Y-%m-%d"),
+            "$lte": datetime.datetime.strptime(end_date, "%Y-%m-%d")
+        }
+    }
+    projection = {"_id": 0, "features": 1, "createdDate": 1, "goals": 1, "plans": 1, "attr": 1}
+    documents = collection.find(query, projection)
+    print(f"Collection: {collection_name}")
+    if collection.count_documents(query) == 0:
+        print("No documents found in the specified date range.")
+    else:
+        for doc in documents:
+            filtered_doc = filter_empty_fields(doc)
+            print(json.dumps(filtered_doc, ensure_ascii=False, indent=4, cls=JSONEncoder))
+
+def read_nursingdiagnosisrecords(patient_id, start_date, end_date):
+    collection_name = "nursingdiagnosisrecords"
+    collection = database[collection_name]
+    query = {
+        "patient": ObjectId(patient_id),
+        "createdDate": {
+            "$gte": datetime.datetime.strptime(start_date, "%Y-%m-%d"),
+            "$lte": datetime.datetime.strptime(end_date, "%Y-%m-%d")
+        }
+    }
+    projection = {"_id": 0, "evaluation": 1, "goals": 1, "createdDate": 1}
+    documents = collection.find(query, projection)
+    print(f"Collection: {collection_name}")
+    if collection.count_documents(query) == 0:
+        print("No documents found in the specified date range.")
+    else:
+        for doc in documents:
+            filtered_doc = filter_empty_fields(doc)
+            print(json.dumps(filtered_doc, ensure_ascii=False, indent=4, cls=JSONEncoder))
+
 if __name__ == "__main__":
     patient_id = read_health_data()
     if patient_id:
-        # 指定查詢的日期範圍
-        start_date = "2019-06-14"
-        end_date = "2019-06-18"
+        start_date = "2018-03-01"
+        end_date = "2018-03-02"
         read_vital_signs(patient_id, start_date, end_date)
         read_nursingnote(patient_id, start_date, end_date)
         read_nursingnotedetails(patient_id, start_date, end_date)
+        read_nursingdiagnoses(patient_id, start_date, end_date)
+        read_nursingdiagnosisrecords(patient_id, start_date, end_date)
